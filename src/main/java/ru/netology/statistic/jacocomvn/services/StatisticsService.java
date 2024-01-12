94c3ec1e9fecbfb3e82bc091811c05790111992b
Step X <stepan170297@gmail.com>
2024-01-12 16:16:13 +0400

    New tests were add

--- src/main/java/ru/netology/statistic/jacocomvn/services/StatisticsService.java
+++ src/main/java/ru/netology/statistic/jacocomvn/services/StatisticsService.java
@@ -1,15 +1,78 @@
 package ru.netology.statistic.jacocomvn.services;
 
-public class StatisticsService{
-public long findMax(long[] incomes) {
-    long currentMax = incomes[0];
-    for (long income : incomes) {
-        if (currentMax < income) {
-            currentMax = income;
+public class StatisticsService {
+
+    public long findMin(long[] incomes) {
+        long currentMin = incomes[0];
+        for (long income : incomes) {
+            if (currentMin > income) {
+                currentMin = income;
+            }
         }
+        return currentMin;
     }
-    return currentMax;
-}
+
+    public long sum(long[] incomes) {
+        long currentSum = 0;
+        for (long income : incomes) {
+            currentSum += income;
+        }
+        return currentSum;
+    }
+
+    public long average(long[] incomes) {
+        long currentAverage = 0;
+        for (long income : incomes) {
+            currentAverage += income;
+        }
+        long averageIncome = currentAverage / 11;
+        return averageIncome;
+    }
+
+    public int calcNumberMoreAverage(long[] incomes) {
+        int count = 0;
+        long averageIncome = average(incomes);
+        for (long income : incomes) {
+            if (income > averageIncome) {
+                count++;
+            }
+        }
+        return count;
+
+
+    }
+
+
+    public int calcNumberBelowAverage(long[] incomes) {
+        int count = 0;
+        long averageIncome = average(incomes);
+        for (long income : incomes) {
+            if (income < averageIncome) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public long calcPercent(long[] incomes) {
+        long currentPercent = 0;
+        long percent = 5;
+        for (long income : incomes) {
+            currentPercent += income;
+        }
+        long percentIncome = currentPercent * percent /100;
+        return percentIncome;
+    }
+
+
+
+
+
+
+
+
+
 }
 
 
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -8,15 +8,69 @@ import ru.netology.statistic.jacocomvn.services.StatisticsService;
 
 public class StatisticsServiceTest {
 
+
     @Test
-    void findMax() {
+    void findMin() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long expected = 3;
 
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMin(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void sum() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12 + 5 + 8 + 4 + 5 + 3 + 8 + 6 + 11 + 11 + 12;
+        long actual = service.sum(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+@Test
+    void average() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = (12 + 5 + 8 + 4 + 5 + 3 + 8 + 6 + 11 + 11 + 12) / 11;
+        long actual = service.average(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+@Test
+    void calcNumberMoreAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long actual = service.calcNumberMoreAverage(incomes);
+        long expected =6;
+    }
+
+
+    @Test
+    void calcNumberBelowAverage() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long actual = service.calcNumberBelowAverage(incomes);
+        long expected =5;
+    }
+
+
+    @Test
+    void calcPercent() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = (12 + 5 + 8 + 4 + 5 + 3 + 8 + 6 + 11 + 11 + 12) *5/ 100;
+        long actual = service.calcPercent(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
